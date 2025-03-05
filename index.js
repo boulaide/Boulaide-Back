7e7db24be552b8047e2918cbaae8b7e1901f53a3
Guilherme Santos <guilherme.garcia1240@gmail.com>
2025-03-05 14:29:49 -0300

    new paths and querys

--- index.js
+++ index.js
@@ -106,6 +106,96 @@ async function addUserCustomization(user_id, customization_id) {
   }
 }
 
+async function getUserStars(user_id) {
+  try {
+    const pool = await getPool();
+    const result = await pool.request().input("userIdParam", sql.Int, user_id)
+      .query(`
+        SELECT us.id AS star_id, us.user_id, q.quest_id, q.title, q.description
+        FROM dbo.user_stars us
+        INNER JOIN dbo.quests q ON us.quest_id = q.quest_id
+        WHERE us.user_id = @userIdParam
+      `);
+
+    return result.recordset.map((star) => ({
+      star_id: star.star_id,
+      user_id: star.user_id,
+      quest: {
+        id: star.quest_id,
+        title: star.title,
+        description: star.description,
+      },
+    }));
+  } catch (err) {
+    console.error("Erro ao buscar estrelas:", err);
+    throw err;
+  }
+}
+
+async function addUserStar(user_id, quest_id) {
+  try {
+    const pool = await getPool();
+    const result = await pool
+      .request()
+      .input("userIdParam", sql.Int, user_id)
+      .input("questIdParam", sql.Int, quest_id).query(`
+        INSERT INTO dbo.user_stars (user_id, quest_id) VALUES (@userIdParam, @questIdParam)
+      `);
+
+    return result.rowsAffected[0] > 0;
+  } catch (err) {
+    console.error("Erro ao adicionar estrela:", err);
+    throw err;
+  }
+}
+
+async function getUserQuests(user_id) {
+  try {
+    const pool = await getPool();
+    const result = await pool.request().input("userIdParam", sql.Int, user_id)
+      .query(`
+        SELECT uq.user_id, uq.quest_id, uq.status, q.title, q.description
+        FROM dbo.user_quests uq
+        INNER JOIN dbo.quests q ON uq.quest_id = q.quest_id
+        WHERE uq.user_id = @userIdParam
+      `);
+
+    return result.recordset.map((quest) => ({
+      user_id: quest.user_id,
+      quest: {
+        quest_id: quest.quest_id,
+        title: quest.title,
+        description: quest.description,
+      },
+      status: quest.status,
+    }));
+  } catch (err) {
+    console.error("Erro ao buscar questões do usuário:", err);
+    throw err;
+  }
+}
+
+async function updateQuestStatus(user_id, quest_id, newStatus) {
+  try {
+    const pool = await getPool();
+    const result = await pool
+      .request()
+      .input("userIdParam", sql.Int, user_id)
+      .input("questIdParam", sql.Int, quest_id)
+      .input("newStatusParam", sql.Int, newStatus).query(`
+        UPDATE dbo.user_quests
+        SET status = @newStatusParam
+        WHERE user_id = @userIdParam AND quest_id = @questIdParam
+      `);
+
+    // Retorna true se pelo menos uma linha foi afetada
+    return result.rowsAffected[0] > 0;
+  } catch (err) {
+    console.error("Erro ao atualizar o status da quest:", err);
+    throw err;
+  }
+}
+
 app.post("/add-customization", async (req, res) => {
   try {
     const { user_id, customization_id } = req.body;
@@ -162,7 +252,10 @@ app.get("/inventory/:id", async (req, res) => {
   try {
     const { id } = req.params;
 
-    if (!id) return res.status(400).json({ success: false, message: "Missing id parameter" });
+    if (!id)
+      return res
+        .status(400)
+        .json({ success: false, message: "Missing id parameter" });
 
     const customizations = await getCustomization(id);
     const equippedItems = customizations.filter((item) => item.equipped);
@@ -174,6 +267,85 @@ app.get("/inventory/:id", async (req, res) => {
   } catch (err) {
     console.error("Error querying Azure SQL:", err);
     res.status(500).send("Error querying database");
+  }
+});
+
+app.get("/stars/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id)
+      return res
+        .status(400)
+        .json({ success: false, message: "Missing id parameter" });
+
+    const stars = await getUserStars(id);
+
+    res.json(stars);
+  } catch (err) {
+    console.error("Error querying Azure SQL:", err);
+    res.status(500).send("Error querying database");
+  }
+});
+
+app.post("/add-user-star", async (req, res) => {
+  try {
+    const { user_id, quest_id } = req.body;
+
+    if (!user_id || !quest_id)
+      return res.status(400).json({ error: "Parâmetros inválidos" });
+
+    const success = await addUserStar(user_id, quest_id);
+
+    if (success)
+      return res.json({
+        success: true,
+        message: "Estrela adicionada com sucesso",
+      });
+    else return res.status(500).json({ error: "Erro ao adicionar estrela" });
+  } catch (err) {
+    console.log("Erro no endpoint de adicionar estrela:", err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get("/user-quests/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id)
+      return res
+        .status(400)
+        .json({ success: false, message: "Missing id parameter" });
+
+    const stars = await getUserQuests(id);
+
+    res.json(stars);
+  } catch (err) {
+    console.error("Error querying Azure SQL:", err);
+    res.status(500).send("Error querying database");
+  }
+});
+
+app.put("/user-quests-status", async (req, res) => {
+  try {
+    const { user_id, quest_id, newStatus } = req.body;
+    console.log(user_id, quest_id, newStatus);
+    if (!user_id || !quest_id || newStatus === undefined)
+      return res.status(400).json({ error: "Parâmetros inválidos" });
+
+    const success = await updateQuestStatus(user_id, quest_id, newStatus);
+
+    if (success)
+      return res.json({
+        success: true,
+        message: "Status da quest mudado com sucesso",
+      });
+    else
+      return res.status(500).json({ error: "Erro ao mudar o status da quest" });
+  } catch (err) {
+    console.log("Erro no endpoint de mudar o status da quest:", err);
+    res.status(500).json({ error: err.message });
   }
 });
 
